aca16cd4360d5f843b6d0948ab1e7ed34df4517a
Ayam Rembo <123289513+ayamrembo@users.noreply.github.com>
2023-02-26 23:45:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys, os
 
 if __name__ == "__main__":
   try:
-      #os.system("git pull")
+      os.system("git pull")
       __import__("rembo").menu()
   except Exception as e:
     exit(str(e))
